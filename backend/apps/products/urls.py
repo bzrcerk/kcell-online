b1ab3288c26d82bc65671d7e59b23f04367fcdea
bzrcerk <bzrcerk@MacBook-Air-bzrcerk.local>
2026-06-03 16:33:48 +0500

    refactor/deleted-front(angular)

--- backend/apps/products/urls.py
+++ backend/apps/products/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductAPIView, CategoryAPIView
+from .views import ProductAPIView, CategoryAPIView, catalog_page
 
 router = DefaultRouter()
 router.register(r'products', ProductAPIView, basename='products')
 router.register(r'categories', CategoryAPIView, basename='categories')
 
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', catalog_page, name='catalog_page')
 ]
--- backend/apps/products/views.py
+++ backend/apps/products/views.py
@@ -1,8 +1,13 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 
+
+def catalog_page(request):
+    products = Product.objects.select_related('category').all()
+    return render(request, 'templates/catalog.html', {'products': products})
 
 # Create your views here.
 class CategoryAPIView(viewsets.ModelViewSet):
